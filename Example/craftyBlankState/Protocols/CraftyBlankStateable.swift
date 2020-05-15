5d29e1934b5db3b955776b38eb0889c629bf48f7
lyle <lyle@craftycoders.io>
2020-05-15 13:37:28 -0500

    added boiler plate code

---
+++ Example/craftyBlankState/Protocols/CraftyBlankStateable.swift
@@ -0,0 +1,13 @@
+//
+//  CraftyBlankStateable.swift
+//  craftyBlankState_Tests
+//
+//  Created by Lyle Jover on 5/15/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+protocol CraftyBlankStatable {
+    var blankStateView: CraftyBlankStateView! { get set }
+}
